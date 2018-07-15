f7473cac11be110ea2fa49b4472e5563127690bb
Stefan Boroda <imnotashrimp@gmail.com>
2018-07-15 09:51:51 +0300

    consolidate js into one jquery function

--- _source/js/logz-docs.js
+++ _source/js/logz-docs.js
@@ -1,48 +1,39 @@
-// Auto-structure .info-box divs
 $( function() {
-  $('div.info-box').wrapInner('<p></p>');
-});
+// Auto-structure .info-box divs
+$('div.info-box').wrapInner('<p></p>');
 
 // Show/hide animations for left-side TOC. Also animates caret icon.
-$( function() {
-  $('ul.toc-child > li.toc-active').find('ul.toc-grandchild').show().addClass('show').siblings('span.toc-heading').find('span.collapse-button').addClass('show');
-  $('ul.toc-grandchild > li.toc-active').parent('ul.toc-grandchild').show().addClass('show').siblings('span.toc-heading').find('span.collapse-button').addClass('show');
+$('ul.toc-child > li.toc-active').find('ul.toc-grandchild').show().addClass('show').siblings('span.toc-heading').find('span.collapse-button').addClass('show');
+$('ul.toc-grandchild > li.toc-active').parent('ul.toc-grandchild').show().addClass('show').siblings('span.toc-heading').find('span.collapse-button').addClass('show');
 
-  $('ul.toc-child').find('li.has-child > span.toc-heading').on('click', function() {
-    let submenu = $(this).siblings('ul.toc-grandchild');
-    let caret = $(submenu).siblings('span.toc-heading').find('span.collapse-button');
+$('ul.toc-child').find('li.has-child > span.toc-heading').on('click', function() {
+  let submenu = $(this).siblings('ul.toc-grandchild');
+  let caret = $(submenu).siblings('span.toc-heading').find('span.collapse-button');
 
-    if (submenu.hasClass('show') == true) {
-      submenu.slideUp().removeClass('show');
-      caret.removeClass('show');
-    } else {
-      submenu.slideDown().addClass('show');
-      caret.addClass('show');
-    }
-  });
-
+  if (submenu.hasClass('show') == true) {
+    submenu.slideUp().removeClass('show');
+    caret.removeClass('show');
+  } else {
+    submenu.slideDown().addClass('show');
+    caret.addClass('show');
+  }
 });
 
 // tablesorter
-$( function() {
-  $('table').tablesorter({ sortList: [[0,0]] });
-});
+$('table').tablesorter({ sortList: [[0,0]] });
 
 // accordion
-$( function() {
-  $( '.accordion' ).accordion({
-    active: false,
-    collapsible: true,
-    heightStyle: 'content',
-    icons: { 'header': 'ui-icon-caret-1-e', 'activeHeader': 'ui-icon-caret-1-s' }
-  });
-} );
+$( '.accordion' ).accordion({
+  active: false,
+  collapsible: true,
+  heightStyle: 'content',
+  icons: { 'header': 'ui-icon-caret-1-e', 'activeHeader': 'ui-icon-caret-1-s' }
+});
 
-// accordion test to expand the section you're writing. comment this out when you're done writing.
-// $( function() { $('.accordion').accordion({ active: 3 }) });
 
-$( function() {
-  $('#toc-collapse-btn').click( function() {
-    $('.toc-container').toggleClass('toc-visibility');
-  });
+// toc collapse button for smaller screens
+$('#toc-collapse-btn').click( function() {
+  $('.toc-container').toggleClass('toc-visibility');
+});
+
 });

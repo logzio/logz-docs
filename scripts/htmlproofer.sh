b0df8444e0b4196cf10d007c771fface1fe4910f
Shalom Boroda <imnotashrimp@gmail.com>
2019-03-05 06:17:26 +0200

    tweak htmlproofer command

--- scripts/htmlproofer.sh
+++ scripts/htmlproofer.sh
@@ -1,5 +1,8 @@
 OUTPUT=$PWD/_site
 
 echo Testing output with HTMLProofer...
-htmlproofer --extension .html --url_ignore "/\/api#operation\/.+/" --http_status_ignore "999" $OUTPUT
+htmlproofer $OUTPUT \
+  --extension .html \
+  --url_ignore "/\/api#operation\/.+/" \
+  --http_status_ignore "999"
 echo '  HTMLProofer done'

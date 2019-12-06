53828ef0c4611203a3297815ab42e426b4dbaa1e
Shalom Boroda <imnotashrimp@gmail.com>
2019-12-06 15:47:19 +0200

    update regex script for new formatting

--- kramdown-to-gfm.js
+++ kramdown-to-gfm.js
@@ -1,54 +1,31 @@
 var gulp = require('gulp');
 var replace = require('gulp-string-replace');
 var sourceFile = process.env.SOURCE;
+var taskStepNum = 0;
 
 gulp.task('kramdown-to-gfm', function() {
   gulp.src([sourceFile]) // Any file globs are supported
-    .pipe(replace(/(---[\s\S]+?---(\n){1,})/, '')) // remove frontmatter
-    .pipe(replace(/^#{5}(?= \b)/gm, '###')) // replace h5 with h3
-    .pipe(replace(/^#{6}(?= \b)/gm, '###')) // replace h6 with h3
+    .pipe(replace(/(---[\s\S]+?---(\n){1})/, (replacement) => {
+      var result = replacement
+        .match(/(?<=^title: ).+$/m);
+      return '# ' + result + '\n';
+    })) // remove frontmatter
     .pipe(replace(/{:.override.btn-img}(\n?)/g, '')) // remove
 
     // clean up the tasklist
-    .pipe(replace(/^1\.(.+\n+)+{:.*tasklist.*}/gm, function(replacement) {
-      var result = replacement
-        .replace(/\n{:.tasklist.firstline-headline}/, '') // remove
-        .replace(/^(?=\d+.)/gm, '#### ') // convert to h4
-        .replace(/^( )+/gm, '') // outdent the indents
-
-      return result;
+    .pipe(replace(/((?<=^#{5} ).+$|^<div class="tasklist">$)/gm, (source) => {
+      switch (source) {
+        case '<div class="tasklist">':
+          taskStepNum = 0;
+          return source;
+        default:
+          taskStepNum++;
+          return taskStepNum + '. ' + source;
+        }
     }))
 
     // convert deflists to table
-    .pipe(replace(/(.*)\n: ((?:.+(?:\n|$))*?)+(?=\n|.*\n:|$)/gm, function(replacement) {
-      result = '<!-- definition:start -->\n' + replacement + '<!-- definition:end -->';
-      result = result
-        .replace(/(?<=<!-- definition:start -->\n)^/gm, '| ')
-        .replace(/$(?=\n<!-- definition:end -->)/gm, ' |')
-        .replace(/^:( )?/gm, ' | ')
-        .replace(/(?<!<!-- definition:(start|end) -->)\n(?!<!-- definition:(start|end) -->)/g, '');
-      return result;
-    }))
-
-    // combine deflists into a single table, add header
-    .pipe(replace(/<!-- definition:start -->\n(.*\n)+?<!-- definition:end -->(?!\n<!-- definition:start -->)/g, function(replacement) {
-      var result = replacement
-        .replace(/<!-- definition:end -->/g, '')
-        .replace(/<!-- definition:start -->/, '| Parameter | Description |\n|---|---|\n')
-        .replace(/<!-- definition:start -->\n/g, '')
-        .replace(/\n{2}/g, '\n');
-
-      return result;
-    }))
-
-    // reformat inline headings
-    .pipe(replace(/.+\n{:( )?.inline-header( )?}/g, function(replacement) {
-      var result = replacement
-        .replace(/\n{:( )?.inline-header( )?}/, '')
-        .replace(/^.+$/m, '**$&**');
-
-      return result;
-    }))
+    .pipe(replace(/{:.paramlist}\n/g, ''))
 
     // reformat info-boxes
     .pipe(replace(/(?<=\n\n).*\n\s*{:.info-box.*}/g, function(replacement) {
@@ -78,18 +55,11 @@ gulp.task('kramdown-to-gfm', function() {
       return result;
     }))
 
-    // move required & default params to the other side of the pipe
-    .pipe(replace(/<span.*?>.*<\/span> \|/g, function(replacement) {
-      result = replacement
-        .replace(/(.+) \|/g, '| $1');
-
-      return result;
-    }))
-    .pipe(replace(/<span.*required-param.*\/span>/g, '**Required**.'))
+    .pipe(replace(/<span.*required-param.*\/span>/g, '(Required)'))
     .pipe(replace(/<span.*default-param.*\/span>/g, function(replacement){
       var stripped = replacement.replace(/<(\/*)span.*?>/g, '');
 
-      var result = '**Default**: ' + stripped + ' <br> '
+      var result = '(Default: ' + stripped + ')'
       return result;
     }))
 
@@ -98,6 +68,7 @@ gulp.task('kramdown-to-gfm', function() {
     .pipe(replace(/{%.*replace-vars.*listener=true.*%}( \\\\)?/g, 'Replace `<<LISTENER-HOST>>` with your region\'s listener host (for example, `listener.logz.io`). For more information on finding your account\'s region, see [Account region](https://docs.logz.io/user-guide/accounts/account-region.html).'))
 
     .pipe(replace(/\\{2}/g, '<br>'))
+    .pipe(replace(/{{site.baseurl}}/g, 'https://docs.logz.io'))
 
     .pipe(gulp.dest('./convert-md'))
 });
